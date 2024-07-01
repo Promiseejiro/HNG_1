e7f4720f9a39c8aada7efec014790390d4aee828
Your Name <promiseejiro43@gmail.com>
2024-07-01 15:46:19 +0100

    new commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const slackNameElement = document.querySelector('[data-testid="slackName"]');
+const slackEmailElement = document.querySelector(
+  '[data-testid="slackEmailElement"]'
+);
+const profileImage = document.querySelector('[data-testid="profile-image"]');
+const currentTimeElement = document.querySelector(
+  '[data-testid="currentTimeElement"]'
+);
+const currentDayElement = document.querySelector(
+  '[data-testid="currentDayElement"]'
+);
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const date = new Date();
+console.log(date.toUTCString());
+profileImage.src = "./assets/slackProfileImage.jpg";
+slackNameElement.textContent = "Promiseejiro";
+slackEmailElement.textContent = "promiseejiro43@gmail.com";
+currentTimeElement.textContent = date.toUTCString();
+currentDayElement.textContent = daysOfWeek[date.getDay()];
